dedc920b2bb04aa6720be2a321c8cabf1760947c
Guilherme Teixeira <4645845+gateixeira@users.noreply.github.com>
2024-02-19 16:24:03 +0100

    Fix test path

--- executar-teste-local.sh
+++ executar-teste-local.sh
@@ -3,7 +3,7 @@
 # Use este script para executar testes locais
 
 RESULTS_WORKSPACE="$(pwd)/load-test/user-files/results"
-GATLING_BIN_DIR=/Users/gateixeira/Downloads/gatling-charts-highcharts-bundle-3.10.3/bin
+GATLING_BIN_DIR="$(pwd)/gatling-charts-highcharts-bundle-3.10.3/bin"
 GATLING_WORKSPACE="$(pwd)/load-test/user-files"
 
 runGatling() {
